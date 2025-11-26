e69e12129a4066b4d32b5b1cca3610b5dbfd18e6
Evita Ramotar <183926691+Evita000@users.noreply.github.com>
2025-11-26 18:24:20 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -25,7 +25,7 @@ let tiltX = 0;
 let tiltY = 0;
 let lastGamma = 0;
 let lastBeta = 0;
-let needsStart = true;   // ← YOU ARE MISSING THIS LINE
+
 
 
 // ---- Motion Permission Helpers ----
@@ -1515,6 +1515,7 @@ function drawDiscoScene() {
 }
 
 // ✅ End of Eggzee Script — all good!
+
 
 
 
